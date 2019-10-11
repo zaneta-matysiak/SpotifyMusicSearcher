c19419f562602fe4bdb75983763c7d9b1dda50e0
Żaneta Matysiak <zaneta.matysiak@currenda.pl>
2019-10-12 00:40:57 +0200

    change constant mongo properties to values from application.properties

--- src/main/java/pl/zanettj/musicSearcher/MongoConfig.java
+++ src/main/java/pl/zanettj/musicSearcher/MongoConfig.java
@@ -2,24 +2,37 @@ package pl.zanettj.musicSearcher;
 
 import com.mongodb.MongoClient;
 import cz.jirutka.spring.embedmongo.EmbeddedMongoFactoryBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.io.IOException;
 
 @Configuration
+@ComponentScan(basePackages = { "pl.zanettj.musicSearcher.*" })
+@PropertySource("classpath:application.properties")
 public class MongoConfig {
 
-    private static final String MONGO_DB_URL = "localhost";
-    private static final String MONGO_DB_NAME = "spotify_music_searcher";
+    @Value("${mongo.host}")
+    private String mongoDbUrl;
+    @Value("${mongo.database}")
+    private String mongoDbName;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @Bean
     public MongoTemplate mongoTemplate() throws IOException {
         EmbeddedMongoFactoryBean mongo = new EmbeddedMongoFactoryBean();
-        mongo.setBindIp(MONGO_DB_URL);
+        mongo.setBindIp(mongoDbUrl);
         MongoClient mongoClient = mongo.getObject();
-        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, MONGO_DB_NAME);
+        MongoTemplate mongoTemplate = new MongoTemplate(mongoClient, mongoDbName);
         return mongoTemplate;
     }
 
